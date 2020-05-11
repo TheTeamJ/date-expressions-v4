ffeabd069aad5ff7cc4029502fb606b32a252fcc
daiiz <da.iz.mg@gmail.com>
2020-05-12 07:57:48 +0900

    ふるい

--- src/mutations/index.js
+++ src/mutations/index.js
@@ -1,14 +1,66 @@
-const { basicMutations } = require('./basic')
+const moment = require('moment-timezone')
+const { debug } = require('../../src0/lib')
 
-const getReservedExpressions = () => {
-  return basicMutations.map(item => item[0])
+const now = moment.tz('Asia/Tokyo')
+const base = { y: now.year(), m: now.month() + 1, d: now.date(), h: now.hour() }
+debug(base)
+
+// 数値指定されているunitの要素数を2にするなど
+// 増減量表現でない場合は数値型に変換する
+const fmt = mutation => {
+  for (const unit in mutation) {
+    const value = mutation[unit]
+    if (Array.isArray(value)) {
+      if (value.length === 0 || value.length > 2) throw new Error(`Invalid value: ${unit}`)
+      if (value.length === 1) {
+        mutation[unit] = [value[0], value[0]]
+      }
+      for (let i = 0; i < 2; i++) {
+        const value = `${mutation[unit][i]}`
+        if (!value.startsWith('+') && !value.startsWith('-')) mutation[unit][i] = parseInt(value)
+      }
+    }
+  }
+  debug(mutation)
+  return mutation
 }
 
-const getDefaultMutations = () => {
-  return basicMutations
+// ユーザ定義のmutから、仕様上妥当なunitだけを拾い出す
+const createMutaion = (mut, defaultValue) => {
+  const mutation = {
+    y: mut.y || defaultValue,
+    m: mut.m || defaultValue,
+    d: mut.d || defaultValue,
+    h: mut.h || defaultValue
+  }
+  return mutation
+}
+
+// weak inherit
+// 絶対表現に用いる型
+const a = mutations => {
+  const res = []
+  for (const mut of mutations) {
+    const mutation = fmt(createMutaion(mut, 'i'))
+    mutation._kind = 'a'
+    res.push(mutation)
+  }
+  return res
+}
+
+// strong inherit
+// 相対表現に用いる型
+const r = mutations => {
+  const res = []
+  for (const mut of mutations) {
+    const mutation = fmt(createMutaion(mut, 'I'))
+    mutation._kind = 'r'
+    res.push(mutation)
+  }
+  return res
 }
 
 module.exports = {
-  getReservedExpressions,
-  getDefaultMutations
+  a,
+  r
 }
