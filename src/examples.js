3144f57f2f7dcb8f81a65618b22bbe044e463fce
daiiz <da.iz.mg@gmail.com>
2020-05-19 04:40:06 +0900

    process.env.TEXTを渡せる

--- src/examples.js
+++ src/examples.js
@@ -1,7 +1,7 @@
 const DateExp = require('./')
 const { debug } = require('./lib')
 
-const dateExp = new DateExp('ここ2年/今ごろ/5日', 'Asia/Tokyo')
+const dateExp = new DateExp(process.env.TEXT || 'ここ2年/今ごろ/5日', 'Asia/Tokyo')
 const momentRanges = dateExp.resolve().momentRanges
 debug('OR: _mergedMutations:')
 debug(dateExp._mergedMutations)
