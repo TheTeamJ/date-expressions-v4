eb5d70d763ca102de436beb30542727842440e63
daiiz <da.iz.mg@gmail.com>
2020-05-21 00:47:31 +0900

    examplesでもregisterSampleCustomMutationsを実行しておく

--- examples.js
+++ examples.js
@@ -1,5 +1,8 @@
 const DateExp = require('./src/')
+const { registerSampleCustomMutations } = require('./test/custom')
 const { debug } = require('./src/lib')
+
+registerSampleCustomMutations()
 
 const dateExp = new DateExp(process.env.TEXT || 'ここ2年/今ごろ/5日', 'Asia/Tokyo')
 const momentRanges = dateExp.resolve().momentRanges
